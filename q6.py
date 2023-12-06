c18da31ac13e9cfc0767891a57bc61ebc13c2da7
Tim Cerexhe <tim@timothyc.info>
2023-12-06 20:23:54 +1100

    optimised q6

--- q6.py
+++ q6.py
@@ -1,4 +1,3 @@
-import math
 from dataclasses import dataclass
 
 @dataclass
@@ -32,17 +31,60 @@ def read_data() -> (list[Record], Record):
         combined = Record(time=ctime, distance=cdistance)
         return [ Record(time=time, distance=distance) for time, distance in zip(times, distances) ], combined
 
+def check(target_speed: int, r: Record) -> bool:
+    race_time = r.time - target_speed
+    distance = race_time * target_speed
+    return distance > r.distance
+
+def search(lo: int, hi: int, r: Record, target: bool) -> int:
+    v = (lo + hi) // 2
+    # print(f'search {lo} - ({v}) - {hi} for {target}')
+    if check(v, r) == target:
+        if check(v-1, r) != target:  # we found the start of the target-valued run!
+            return v
+        else: #  we're somewhere in the middle of right section, so search left
+            return search(lo, v, r, target)
+
+    else:  # we're in the wrong section, search right
+        return search(v, hi, r, target)
+        
+
 def get_winning_combos(r: Record) -> int:
-    speed = int(r.distance / r.time)
-    combos = 0
-    while speed < r.time:
-        speed += 1
-        race_time = r.time - speed
-        distance = race_time * speed
-        if distance > r.distance:
-            combos += 1
-    #print(r, '=', combos)
-    return combos
+    # TODO(tim) this seems like a hack, but it works:
+    # winning combos for Record(time=42, distance=284): [(False, 7, 8), (True, 9, 33), (False, 34)]
+    # winning combos for Record(time=68, distance=1005): [(False, 15, 21), (True, 22, 46), (False, 47)]
+    # winning combos for Record(time=69, distance=1122): [(False, 17, 26), (True, 27, 42), (False, 43)]
+    # winning combos for Record(time=85, distance=1341): [(False, 16, 20), (True, 21, 64), (False, 65)]
+    split = r.time // 2 
+
+    start = search(0, split, r, True)
+    end = search(split, r.time, r, False)
+    return end - start
+
+# def og_get_winning_combos(r: Record) -> int:
+#     seq = []
+#     current = None
+#     combos = 0
+#     speed = int(r.distance / r.time)
+#     while speed < r.time:
+#         speed += 1
+#         race_time = r.time - speed
+#         distance = race_time * speed
+#         success = False
+#         if distance > r.distance:
+#             success = True
+#             combos += 1
+
+#         if not current or current[0] != success:
+#             if current:
+#                 current_success, start = current
+#                 seq.append((current_success, start, speed - 1))
+#             current = (success, speed)
+#     if current:
+#         seq.append(current)
+
+#     print(f'winning combos for {r}: {seq}')
+#     return combos
 
 data, combined = read_data()
 
@@ -50,5 +92,10 @@ combos = [ get_winning_combos(r) for r in data ]
 combo_product = 1
 for c in combos:
     combo_product *= c
-print(combo_product) #, '(expect 288)')
-print(get_winning_combos(combined))
+print(combo_product)
+assert combo_product == 440000
+
+single = get_winning_combos(combined)
+print(single)
+
+assert single == 26187338
